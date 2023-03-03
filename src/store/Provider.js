217a3bc5eebeabc3ab7c604cec52a46d7f3fcaca
An Vong <an.vong@cdu.edu.au>
2023-03-03 12:59:52 +0930

    remove logger for avoid confusion

--- src/store/Provider.js
+++ src/store/Provider.js
@@ -1,12 +1,12 @@
 import { useReducer } from 'react'
 import Context from './Context'
 import reducer, {initState} from './reducer'
-import logger from './logger'
+// import logger from './logger'
 
 // provider component using reducer
 function Provider({children}) {
     // logger is use to call reducer for logging 
-    const [state, dispatch] = useReducer(logger(reducer), initState)
+    const [state, dispatch] = useReducer(reducer, initState)
     return (
         <Context.Provider value={[state, dispatch]}>
             {children}
